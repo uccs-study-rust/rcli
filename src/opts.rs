760884c94d5ce9c85236032b2dd3153a2a8bc30e
uccs <1500846601@qq.com>
2025-01-21 19:29:11 +0800

    feature: add csv cli support

---
+++ src/opts.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name="rcli", version, author, about, long_about=None)]
+pub struct Opts {
+    #[command(subcommand)]
+    pub cmd: SubCommand,
+}
+
+#[derive(Parser, Debug)]
+pub enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+#[derive(Parser, Debug)]
+pub struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    pub input: String,
+    #[arg(short, long, default_value = "output.json")]
+    pub output: String,
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    if std::path::Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("File does not exist")
+    }
+}
