713347c687ca76a9c82eb3119f9c2e14b0db0a93
uccs <1500846601@qq.com>
2025-01-22 09:16:41 +0800

    支持 yaml

--- src/opts.rs
+++ src/opts.rs
@@ -1,4 +1,13 @@
+use std::{fmt, str::FromStr};
+
 use clap::Parser;
+
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+    // Toml,
+}
 
 #[derive(Parser, Debug)]
 #[command(name="rcli", version, author, about, long_about=None)]
@@ -16,12 +25,14 @@ pub enum SubCommand {
 pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    #[arg(short, long)]
+    pub output: Option<String>,
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
     #[arg(long, default_value_t = true)]
     pub header: bool,
+    #[arg( long, value_parser=parse_format,default_value = "json")]
+    pub format: OutputFormat,
 }
 
 fn verify_input_file(filename: &str) -> Result<String, &'static str> {
@@ -29,5 +40,38 @@ fn verify_input_file(filename: &str) -> Result<String, &'static str> {
         Ok(filename.into())
     } else {
         Err("File does not exist")
+    }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+            // OutputFormat::Toml => "toml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            // "toml" => Ok(OutputFormat::Toml),
+            _ => Err(anyhow::anyhow!("Invalid format")),
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
