fa7c11e53dc28e858af51f40f0f6b5ca71381245
uccs <1500846601@qq.com>
2025-01-24 15:59:47 +0800

    feature: support http static file serve

---
+++ src/cli/http.rs
@@ -0,0 +1,21 @@
+use std::{fmt, path::PathBuf, str::FromStr};
+
+use clap::Parser;
+
+use super::{verify_file, verify_path};
+
+#[derive(Debug, Parser)]
+pub enum HttpSubCommand {
+    #[command(about = "Serve a directory over HTTP")]
+    Serve(HttpServeOpts),
+}
+
+#[derive(Debug, Parser)]
+pub struct HttpServeOpts {
+    #[arg(short, long, value_parser = verify_path,default_value=".")]
+    pub dir: PathBuf,
+    #[arg(short, long, default_value_t = 8080)]
+    pub port: u16,
+    // #[arg(short, long, default_value = "")]
+    // pub host: String,
+}
